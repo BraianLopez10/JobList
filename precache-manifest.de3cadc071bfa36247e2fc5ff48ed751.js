a6827ac40392375c38291a716de7c08eb7e6194c
Braian Emanuel Lopez <brialopez10089@gmail.com>
2020-07-08 03:12:37 -0300

    Updates

---
+++ precache-manifest.de3cadc071bfa36247e2fc5ff48ed751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4dec1e55912c1f300777ff2f6e2a59",
+    "url": "/JobList/index.html"
+  },
+  {
+    "revision": "3b573a86b21e9f607be1",
+    "url": "/JobList/static/css/main.6f936c38.chunk.css"
+  },
+  {
+    "revision": "db5d387a97304b788987",
+    "url": "/JobList/static/js/2.bc8e227e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JobList/static/js/2.bc8e227e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b573a86b21e9f607be1",
+    "url": "/JobList/static/js/main.930a4a9f.chunk.js"
+  },
+  {
+    "revision": "30b7f2a37576e1057608",
+    "url": "/JobList/static/js/runtime-main.40839dd1.js"
+  }
+]);
